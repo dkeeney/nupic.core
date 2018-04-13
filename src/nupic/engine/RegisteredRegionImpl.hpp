19bac880d715c9a2be0ba60a515c3e10086eee03
David Keeney <dkeeney@gmail.com>
2018-04-13 16:48:38 -0700

    Removing Python stuff from Factory and getting it to compile.

--- src/nupic/engine/RegisteredRegionImpl.hpp
+++ src/nupic/engine/RegisteredRegionImpl.hpp
@@ -32,56 +32,61 @@
 
 #include <string>
 
-namespace nupic {
-struct Spec;
-class BundleIO;
-class RegionImpl;
-class Region;
-class ValueMap;
+namespace nupic
+{
+  struct Spec;
+  class BundleIO;
+  class RegionImpl;
+  class Region;
+  class ValueMap;
 
-class GenericRegisteredRegionImpl {
-public:
-  GenericRegisteredRegionImpl() {}
+  class GenericRegisteredRegionImpl {
+    public:
+      GenericRegisteredRegionImpl() {}
 
-  virtual ~GenericRegisteredRegionImpl() {}
+      virtual ~GenericRegisteredRegionImpl() {}
 
-  virtual RegionImpl *createRegionImpl(const ValueMap &params,
-                                       Region *region) = 0;
+      virtual RegionImpl* createRegionImpl( ValueMap& params, Region *region) = 0;
 
-  virtual RegionImpl *deserializeRegionImpl(BundleIO &params,
-                                            Region *region) = 0;
+      virtual RegionImpl* deserializeRegionImpl( BundleIO& params, Region *region) = 0;
 
-  virtual RegionImpl *deserializeRegionImpl(capnp::AnyPointer::Reader &proto,
-                                            Region *region) = 0;
+      virtual Spec* createSpec() = 0;
 
-  virtual Spec *createSpec() = 0;
-};
+  };
 
-template <class T>
-class RegisteredRegionImpl : public GenericRegisteredRegionImpl {
-public:
-  RegisteredRegionImpl() {}
+  template <class T>
+  class RegisteredRegionImpl: public GenericRegisteredRegionImpl {
+    public:
+      RegisteredRegionImpl() {
+        cachedSpec_ = nullptr;
+      }
 
-  ~RegisteredRegionImpl() {}
+      ~RegisteredRegionImpl() {
+        if (cachedSpec_)
+          delete cachedSpec_;
+      }
 
-  virtual RegionImpl *createRegionImpl(const ValueMap &params,
-                                       Region *region) override {
-    return new T(params, region);
-  }
+      virtual RegionImpl* createRegionImpl( ValueMap& params, Region *region) override
+      {
+        return new T(params, region);
+      }
 
-  virtual RegionImpl *deserializeRegionImpl(BundleIO &params,
-                                            Region *region) override {
-    return new T(params, region);
-  }
+      virtual RegionImpl* deserializeRegionImpl( BundleIO& params, Region *region) override
+      {
+        return new T(params, region);
+      }
 
-  virtual RegionImpl *deserializeRegionImpl(capnp::AnyPointer::Reader &proto,
-                                            Region *region) override {
-    return new T(proto, region);
-  }
+      virtual Spec* createSpec() override
+      {
+        if (!cachedSpec_) {
+          cachedSpec_ = T::createSpec();
+        }
+        return cachedSpec_;
+      }
+  private:
+    Spec * cachedSpec_;
+  };
 
-  virtual Spec *createSpec() override { return T::createSpec(); }
-};
-
-} // namespace nupic
+}
 
 #endif // NTA_REGISTERED_REGION_IMPL_HPP
